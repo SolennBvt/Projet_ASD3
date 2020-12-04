280f0703ba41653c83d89213ceb728a86785d01b
SolennBvt <74281175+SolennBvt@users.noreply.github.com>
2020-12-04 23:04:01 +0100

    Add files via upload

---
+++ Beuvant_Coeurdevey_Projet_ASD3(2)/src/AVL.java
@@ -0,0 +1,277 @@
+
+public class AVL<Type extends Comparable> { // Classe T générique
+
+    Type element; // si null, l'avl est vide
+    int bal; // balance de l'arbre égal à 0, 1 ou -1
+    AVL<Type> left; // fils gauche
+    AVL<Type> right; // fils droit
+
+    // Constructeurs
+    public AVL(){
+        element = null;
+        bal = 0;
+        left = null;
+        right = null;
+    }
+    private AVL(Type value, AVL<Type> left, AVL<Type> right) {
+        this.element = value;
+        this.left = left;
+        this.right = right;
+    }
+
+    // Méthodes
+    public int insert(Type elt) {
+
+        int h;
+
+        if (element == null) {
+
+            element = elt;
+            bal = 0;
+            return 1;
+
+        } else if (elt.compareTo(element) > 0) { // elt > element donc insertion à droite
+
+            if (right == null) { right = new AVL<Type>(); }
+            h = right.insert(elt);
+
+        } else { // elt <= element donc insertion à gauche
+
+            if (left == null) { left = new AVL<Type>(); }
+            h = -left.insert(elt);
+        }
+
+        // Balance et Equilibre
+        if(h == 0){ return 0; }
+        else {
+            bal = bal + h;
+            balance();
+            if(bal == 0){ return 0; } else { return 1; }
+        }
+    }
+    /*
+    public int suppressMin(AVL<Type> father) {
+
+        int h;
+
+        if (left == null) { // on est au minimum
+
+            if(father == null){ // on est à la racine
+
+                if(right != null){
+                    this.element = right.element;
+                    this.bal = right.bal;
+                    this.left = right.left;
+                    this.right = right.right;
+                } else {
+                    this.element = null;
+                }
+            }else if (right !=null){
+
+                father.left = right;
+
+            } else {
+                father.left = null;
+            }
+            return -1;
+
+        } else { // on continue de chercher à gauche
+
+            h = -left.suppressMin(this);
+            if (h == 0) { return 0; }
+            else {
+                bal = bal + h;
+                balance();
+                if(bal == 0){return -1;} else {return 0;}
+            }
+        }
+    }
+
+     */
+
+    public void remove(Type elt){
+        remove(elt, this);
+    }
+    private int remove(Type elt, AVL<Type> father){
+
+        int h;
+
+        if(this.element == null){
+            return 0;
+
+        } else if (elt.compareTo(element) > 0) { // on va à droite
+            h = right.remove(elt, this);
+
+        } else if (elt.compareTo(element) < 0) { // on va à gauche
+            h = -left.remove(elt, this);
+
+        }
+        //on est au noeud à supprimer
+        else {
+            AVL<Type> temp;
+
+            // 0 ou 1 fils
+            if( (left == null) || (right == null) ) {
+
+
+                if (left != null) { // A <--- A.g
+
+                    this.element = left.element;
+                    this.right = left.right;
+                    this.left = left.left;
+
+                } else if (right != null){ // A <--- A.d
+
+                    this.element = right.element;
+                    this.left = right.left;
+                    this.right = right.right;
+
+                } else { // pas d'enfants, le noeud est supprimé
+
+                    if(father.left == this){
+                        father.left = null;
+                    } else {
+                        father.right = null;
+                    }
+                }
+                return -1;
+
+            } else { // 2 fils, A <--- Min(A.d) puis suppressMin(A.d)
+
+                AVL<Type> rightMin = this.right.getMin();
+                this.element = rightMin.element;
+
+                h = this.right.remove(rightMin.element, this);
+            }
+
+        }
+        // Balance et Equilibre
+        if (h == 0) { return 0;}
+        else {
+            bal = bal + h;
+            balance();
+            if(bal == 0) { return -1; } else { return 0; }
+        }
+    }
+
+    /*
+     * Retourne le noeud contenant le minimum de l'AVL
+     */
+    public AVL<Type> getMin(){
+        if(left == null){
+            return this;
+        } else {
+            return left.getMin();
+        }
+    }
+    public int numberOfNodes(){
+        if(left == null || right == null){
+            if(left != null){
+                return 1 + left.numberOfNodes();
+            } else if(right != null) {
+                return 1 + right.numberOfNodes();
+            } else {
+                return 1;
+            }
+        } else {
+            return 1 + left.numberOfNodes() + right.numberOfNodes();
+        }
+    }
+    private void balance(){
+        if(bal == 2){
+            if(right.bal >= 0){
+                rotG();
+            } else {
+                right.rotD();
+                rotG();
+            }
+        } else if (bal == -2){
+            if(left.bal <= 0){
+                rotD();
+            } else {
+                left.rotG();
+                rotD();
+            }
+        }
+    }
+    private void rotG(){
+
+        AVL<Type> temp, B;
+        int a, b;
+
+        temp = this.copy();
+        B = temp.right;
+
+        //initialisation des balances
+        a = this.bal;
+        b = B.bal;
+
+        // rotation
+        temp.right = B.left;
+        B.left = temp;
+
+        //calcul des nouvelles balances
+        temp.bal = a - Math.max(b, 0) - 1;
+        B.bal = Math.min(Math.min(a-2, a+b-2), b-1);
+
+        //this = B
+        this.bal = B.bal;
+        this.element = (Type) B.element;
+        this.right = B.right;
+        this.left = B.left;
+    }
+    private void rotD(){
+
+        AVL<Type> temp, B;
+        int a, b;
+
+        temp = this.copy();
+        B = this.left;
+
+        //initialisation des balances
+        a = this.bal;
+        b = B.bal;
+
+        // rotation
+        temp.left = B.right;
+        B.right = temp;
+
+        //calcul des nouvelles balances
+        temp.bal = a - Math.min(b, 0) + 1;
+        B.bal = Math.max(Math.max(a+2, a+b+2), b+1);
+
+        //this = B
+        this.bal = B.bal;
+        this.element = (Type) B.element;
+        this.right = B.right;
+        this.left = B.left;
+    }
+
+    private AVL copy(){
+
+        AVL clone = new AVL(this.element, this.left, this.right);
+
+        clone.bal = this.bal;
+
+        if(left != null){
+            clone.left = this.left.copy();
+        }
+        if(right != null){
+            clone.right = this.right.copy();
+        }
+        return clone;
+    }
+
+    public String toString(){
+
+        if(left == null && right == null){
+            return element.toString();
+        } else if (left != null && right != null){
+            return element.toString()+ "( " + left.toString() + " " + right.toString() + " )";
+        } else if (right == null){
+            return element.toString() + "( "  + left.toString() + " )";
+        } else {
+            return element.toString()+ "( " + right.toString() + " )";
+        }
+    }
+}
---
+++ Beuvant_Coeurdevey_Projet_ASD3(2)/src/Quadtree.java
@@ -0,0 +1,332 @@
+import java.awt.*;
+
+public class Quadtree implements Comparable {
+
+    private Quadtree childs[];
+    private Color color ;
+    private ImagePNG img;
+    private Quadtree father;
+
+    /*
+     * Construit un quadtree compressé sans dégradation
+     */
+    public Quadtree(ImagePNG img, int x, int y, int length, Quadtree father){
+
+        this.img = img;
+        this.childs = new Quadtree[4];
+        this.father = father;
+
+        //si tous les pixels de la zone concernée sont de la même couleur alors on crée une feuille
+        if(isSameColor(img, x, y, length)){
+
+            this.color = img.getPixel(x, y);
+
+        } else { //sinon création d'un noeud -> on divise la zone en 4 sous-région N0, NE, SE, SO
+
+            int pas = length /2; //taille du sous-carré
+
+            //matrice servant à calculer les nouvelles coordonnées (x, y) des sous-régions
+            int matrice[][] = {{0,0},{1,0},{1,1},{0,1}};
+
+            for(int i = 0; i < 4; i++){
+
+                //création des quatre fils correspondant aux sous-régions
+                this.childs[i] = new Quadtree(img, x + matrice[i][0] * pas, y + matrice[i][1] * pas, pas, this);
+            }
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /*
+     * Retourne si les pixels sont tous de la même couleur dans une sous-partie carrée d'une image
+     *
+     * @param ImagePNG img : image à examiner
+     * @param int x, y : coordonnée (x, y) en haut à gauche du carré
+     * @param int length : taille du côté du carré
+     * @return : booleen
+     */
+    private boolean isSameColor(ImagePNG img, int x, int y, int length){
+
+        Color previousColor = img.getPixel(x, y);
+        Color actualColor;
+        int col = x + 1;
+        int line = y;
+
+        while((col < x + length) && (line < y + length)){
+
+            actualColor = img.getPixel(col, line);
+
+            if(actualColor.getRGB() != previousColor.getRGB()){
+
+                return false;
+
+            } else {
+
+                col++;
+
+                if(col == x + length) {
+
+                    col = x;
+                    line++;
+                }
+            }
+        }
+        return true;
+    }
+
+    /*
+     * Effectue la compression delta avec dégradation du quadtree selon une dégradation maximale autorisée
+     * @param int delta : dégradation maximale autorisée, compris entre 0 et 255 (ou 192? --> deux valeurs différentes dans le sujet)
+     */
+    public void compressDelta(int delta){
+
+        if(this.color == null){ //on est à un noeud
+
+            for(int i = 0; i < 4; i++){ //pour chaque fils
+
+                if(this.childs[i].color == null){ //si le fils est un noeud
+
+                    this.childs[i].compressDelta(delta); //on "descend"
+                }
+            }
+
+            //compression éventuelle
+            if(allSonsAreLeaves()){ //condition 1 : tous les fils sont des feuilles
+
+                double lambda = maxColorimetricDifference();
+
+                if(lambda <= delta){ //condition 2 : écart_colorimétrique_max <= delta |---> compression
+                    transformToLeaf();
+                }
+            }
+        }
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void compressPhi(int phi){
+
+        AVL<Quadtree> colorDiffAVL = new AVL(); // AVL qui contiendra les noeud pères de 4 feuilles
+        Quadtree minColorDiff; // le noeud minimal dde l'AVL
+        int nbLeaves = numberOfLeaves(); // nombre de feuilles du quadtree
+
+        this.compressPhi_rec(colorDiffAVL);
+
+        while(phi < nbLeaves && colorDiffAVL.numberOfNodes() > 0) {
+
+            minColorDiff = colorDiffAVL.getMin().element;
+
+            colorDiffAVL.remove(minColorDiff);
+
+            minColorDiff.transformToLeaf();
+
+            nbLeaves -=3;
+
+            if(minColorDiff.father != null && minColorDiff.father.allSonsAreLeaves()){
+
+                colorDiffAVL.insert(minColorDiff.father);
+            }
+        }
+    }
+
+    /*
+     * Remplis un AVL avec les noeuds du quadtree qui n'ont que des feuilles comme fils
+     */
+    private void compressPhi_rec(AVL<Quadtree> colorDiffAVL){
+
+        if(this.color == null) { // on est à un noeud
+
+            if (allSonsAreLeaves()) { // tous nos fils sont des feuilles
+
+                colorDiffAVL.insert(this); // on insert l'écart colorimétrique max dans l'AVL
+
+            } else {
+
+                for (int i = 0; i < 4; i++) {
+                    childs[i].compressPhi_rec(colorDiffAVL);
+                }
+            }
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /*
+     * Retourne la couleur moyenne des feuilles d'un noeud.
+     * Pré-condition : le quadtree appelant doit être un noeud et tous ses fils des feuilles
+     */
+    private Color mediumColor(){
+
+        double rm = 0, gm = 0, bm = 0;
+
+        for(int i = 0 ; i < 4 ; i++){
+            rm += childs[i].color.getRed();
+            gm += childs[i].color.getGreen();
+            bm += childs[i].color.getBlue();
+        }
+        rm = rm/4;
+        gm = gm/4;
+        bm = bm/4;
+
+        return new Color((int)rm,(int)gm,(int)bm);
+    }
+
+    /*
+     * Transforme un noeud en feuille. Sa couleur devient alors la couleur moyennes de ses fils.
+     * Pré-condition : le quadtree appelant est un noeud et tous ses fils des feuilles.
+     */
+    private void transformToLeaf(){
+
+        color = mediumColor(); // transformation en feuille
+
+        for (int i = 0; i < 4; i++) {
+
+            childs[i] = null; // suppression des fils
+        }
+    }
+
+    /*
+     * Retourne l'écart colorimétrique maximum de ses feuilles.
+     * Pré-condition : le quadtree appelant est un noeud et tous ses fils des feuilles.
+     */
+    private double maxColorimetricDifference(){
+
+        double lambda=0;
+
+        //calcul de la couleur moyenne au noeud
+
+        Color medCol = mediumColor();
+
+        // calcul de l'écart colorimétrique max des feuilles du noeud.
+        // Lambda = max( la_1, la_2, la_3, la_4) avec la_i = rac( 1/3 * ( (ri - rm)² + (gi - gm)² + (bi - bm)² ))
+
+        for(int i = 0; i < 4; i++){
+
+            lambda = Math.max(lambda, Math.sqrt((Math.pow(childs[i].color.getRed() - medCol.getRed(), 2) + Math.pow(childs[i].color.getGreen() - medCol.getGreen(), 2) + Math.pow(childs[i].color.getBlue() - medCol.getBlue(), 2)) /3));
+        }
+
+        return lambda;
+    }
+
+    /*
+     * Retourne vrai si tous les fils sont des feuilles, faux sinon
+     * Pré-condition : le quadtree appelant est un noeud et tous ses fils des feuilles
+     */
+    private boolean allSonsAreLeaves(){
+        return childs[0].color != null && childs[1].color != null && childs[2].color != null && childs[3].color != null;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /*
+     * Retourne le nombre de feuille
+     */
+    public int numberOfLeaves(){
+
+        if(childs[0] == null){ // on est au niveau d'une feuille car un noeud a forcément 4 fils
+
+            return 1;
+
+        } else {
+
+            return childs[0].numberOfLeaves() + childs[1].numberOfLeaves() + childs[2].numberOfLeaves() + childs[3].numberOfLeaves();
+        }
+    }
+
+    /*
+     * Retourne une ImagePNG correspondant au Quadtree
+     */
+    public ImagePNG toPNG(){
+
+        ImagePNG png = new ImagePNG(this.img);
+        modifPNG_rec(png, 0, 0, this.img.height());
+
+        return png;
+    }
+
+    /*
+     * Modifie une image pour qu'elle corresponde au quadtree
+     * @param x, y : entiers représentants la coordonnée (x, y) du point en haut à gauche
+     * @param squareSize : taille du côté du carré de l'image à modifier (en pixel)
+     */
+    private void modifPNG_rec(ImagePNG png, int x, int y, int squareSize){
+
+        if (this.color != null) { // feuille ou noeud compressé
+
+            if (squareSize == 1) { // carré de 1px -> feuille
+                png.setPixel(x, y, this.color);
+
+            } else { // noeud compressé
+
+                int col = x; int line = y;
+                int xMax = x + squareSize - 1;
+                int yMax = y + squareSize - 1;
+
+                while((col < xMax) || (line < yMax)){
+
+                    png.setPixel(col, line, this.color);
+
+                    if(col == xMax){ // on a fini de parcourir la ligne, on passe à la suivante
+                        col = x;
+                        line++;
+
+                    } else { // on continue la ligne
+                        col++;
+                    }
+                }
+                png.setPixel(col, line, this.color);
+
+            }
+        } else { // noeud
+            /*
+            Img divisée en 4 sous-carrés avec une nouvelle taille = ancienne /2, nouvelles coordonnées :
+            NO -> x,y
+            NE -> x + taille, y
+            SE -> x + taille, y + taille
+            SO -> x, y + taille
+             */
+            int matrix[][] = {{0,0},{1,0},{1,1},{0,1}}; // NO -> childs[0], NE -> childs[1], SE-> childs[2], SO-> childs[3]
+            int newSquareSize = squareSize /2;
+
+            for(int i = 0; i < 4; i++) {
+
+                this.childs[i].modifPNG_rec(png, x + matrix[i][0] * newSquareSize, y + matrix[i][1] * newSquareSize, newSquareSize );
+            }
+        }
+    }
+
+    /*
+     * Retourne une chaîne de caractère représentant le quadtree
+     * Pré-condition : quadtree non nul
+     */
+    public String toString(){
+
+        if(childs[0] == null){
+            return ImagePNG.colorToHex(color);
+        } else {
+            return "( " + childs[0].toString() + " " + childs[1].toString() + " " + childs[2].toString() + " " + childs[3].toString() + " )";
+        }
+    }
+
+    /*
+     *  Pour la classe AVL<Quadtree> : comparaison suivant l'écart colorimétrique pour insérer le Quadtree dans l'AVL
+     */
+    @Override
+    public int compareTo(Object o) {
+        if(o instanceof Quadtree){
+            Quadtree q = (Quadtree)o;
+            if(this.maxColorimetricDifference() > q.maxColorimetricDifference()){
+                return 1;
+            } else if(this.maxColorimetricDifference() < q.maxColorimetricDifference()){
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+        return -2;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+}
